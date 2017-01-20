13fe2926bac24c5c91e024be675757bf16c10541
Tomohiro Noguchi (@ovrmrw) <t.ngc.0403@gmail.com>
2017-01-20 13:48:15 +0900

    Fix

--- __test__/index.ts
+++ __test__/index.ts
@@ -1,80 +1,87 @@
+import 'zone.js/dist/zone-node'
+declare const Zone: any
+
 import * as assert from 'assert'
 import { Observable } from 'rxjs'
 
 import { getReactiveStoreAsSingleton, getObjectKeys, LoopType } from '../src'
 
 
-interface AppState {
-  increment: IncrementState,
-  timestamp: number,
-}
+Zone.current.fork({}).run(() => {
 
-interface IncrementState {
-  counter: number,
-}
+  interface AppState {
+    increment: IncrementState,
+    timestamp: number,
+  }
 
-const initialState: AppState = {
-  increment: {
-    counter: 0
-  },
-  timestamp: 0,
-}
+  interface IncrementState {
+    counter: number,
+  }
 
-const KEY = getObjectKeys(initialState)
+  const initialState: AppState = {
+    increment: {
+      counter: 0
+    },
+    timestamp: 0,
+  }
 
-const store = getReactiveStoreAsSingleton(initialState, {
-  concurrent: 1,
-  output: true,
-  loopType: LoopType.asap
-})
+  const KEY = getObjectKeys(initialState)
 
-
-let value: number | undefined
-let timestamp: number | undefined
-
-
-store.getter()
-  .filterByUpdatedKey(KEY.increment)
-  .subscribe(state => {
-    value = state.increment.counter
+  const store = getReactiveStoreAsSingleton(initialState, {
+    concurrent: 1,
+    output: true,
+    loopType: LoopType.asap
   })
 
 
-store.getter()
-  .filterByUpdatedKey(KEY.timestamp)
-  .subscribe(state => {
-    timestamp = state.timestamp
-    console.log('************ Timestamp is updated. ************')
+  let value: number | undefined
+  let timestamp: number | undefined
+
+
+  store.getter()
+    .filterByUpdatedKey(KEY.increment)
+    .subscribe(state => {
+      value = state.increment.counter
+    })
+
+
+  store.getter()
+    .filterByUpdatedKey(KEY.timestamp)
+    .subscribe(state => {
+      timestamp = state.timestamp
+      console.log('************ Timestamp is updated. ************')
+    })
+
+
+  store.setter(KEY.increment, (p) => ({ counter: p.counter + 1 }))
+    .then(() => store.setter(KEY.increment, () => (q) => ({ counter: q.counter + 1 })))
+    .then(() => store.setter(KEY.increment, Promise.resolve((p) => ({ counter: p.counter + 1 }))))
+    .then(() => store.setter(KEY.increment, Promise.resolve((_, a) => ({ counter: a.increment.counter + 1 }))))
+    .then(() => store.setter(KEY.increment, Observable.of((p) => ({ counter: p.counter + 1 }))))
+    .then(() => store.setter(KEY.increment, Observable.of((_, a) => ({ counter: a.increment.counter + 1 }))))
+    .then(() => store.setter(KEY.increment, () => Promise.resolve((q) => ({ counter: q.counter + 1 }))))
+    .then(() => store.setter(KEY.increment, () => Observable.of((q) => ({ counter: q.counter + 1 }))))
+    .then(() => store.setter(KEY.increment, (_, a) => ({ counter: a.increment.counter + 1 })))
+    .then(() => store.setter(KEY.increment, (_, a) => (_, b) => ({ counter: b.increment.counter + 1 })))
+
+
+  store.setter(KEY.increment, (p) => ({ counter: p.counter - 1 }))
+    .then(() => store.setter(KEY.increment, () => (q) => ({ counter: q.counter - 1 })))
+    .then(() => store.setter(KEY.increment, Promise.resolve((p) => ({ counter: p.counter - 1 }))))
+    .then(() => store.setter(KEY.increment, Promise.resolve((_, a) => ({ counter: a.increment.counter - 1 }))))
+    .then(() => store.setter(KEY.increment, Observable.of((p) => ({ counter: p.counter - 1 }))))
+    .then(() => store.setter(KEY.increment, Observable.of((_, a) => ({ counter: a.increment.counter - 1 }))))
+    .then(() => store.setter(KEY.increment, () => Promise.resolve((q) => ({ counter: q.counter - 1 }))))
+    .then(() => store.setter(KEY.increment, () => Observable.of((q) => ({ counter: q.counter - 1 }))))
+    .then(() => store.setter(KEY.increment, (_, a) => ({ counter: a.increment.counter - 1 })))
+    .then(() => store.setter(KEY.increment, (_, a) => (_, b) => ({ counter: b.increment.counter - 1 })))
+    .then(() => store.setter(KEY.timestamp, new Date().getTime()))
+
+
+  setImmediate(() => {
+    assert(value === 0)
+    assert(typeof timestamp === 'number' && timestamp > 0)
+    console.log('[passed all the tests.]')
   })
 
-
-store.setter(KEY.increment, (p) => ({ counter: p.counter + 1 }))
-  .then(() => store.setter(KEY.increment, () => (q) => ({ counter: q.counter + 1 })))
-  .then(() => store.setter(KEY.increment, Promise.resolve((p) => ({ counter: p.counter + 1 }))))
-  .then(() => store.setter(KEY.increment, Promise.resolve((_, a) => ({ counter: a.increment.counter + 1 }))))
-  .then(() => store.setter(KEY.increment, Observable.of((p) => ({ counter: p.counter + 1 }))))
-  .then(() => store.setter(KEY.increment, Observable.of((_, a) => ({ counter: a.increment.counter + 1 }))))
-  .then(() => store.setter(KEY.increment, () => Promise.resolve((q) => ({ counter: q.counter + 1 }))))
-  .then(() => store.setter(KEY.increment, () => Observable.of((q) => ({ counter: q.counter + 1 }))))
-  .then(() => store.setter(KEY.increment, (_, a) => ({ counter: a.increment.counter + 1 })))
-  .then(() => store.setter(KEY.increment, (_, a) => (_, b) => ({ counter: b.increment.counter + 1 })))
-
-
-store.setter(KEY.increment, (p) => ({ counter: p.counter - 1 }))
-  .then(() => store.setter(KEY.increment, () => (q) => ({ counter: q.counter - 1 })))
-  .then(() => store.setter(KEY.increment, Promise.resolve((p) => ({ counter: p.counter - 1 }))))
-  .then(() => store.setter(KEY.increment, Promise.resolve((_, a) => ({ counter: a.increment.counter - 1 }))))
-  .then(() => store.setter(KEY.increment, Observable.of((p) => ({ counter: p.counter - 1 }))))
-  .then(() => store.setter(KEY.increment, Observable.of((_, a) => ({ counter: a.increment.counter - 1 }))))
-  .then(() => store.setter(KEY.increment, () => Promise.resolve((q) => ({ counter: q.counter - 1 }))))
-  .then(() => store.setter(KEY.increment, () => Observable.of((q) => ({ counter: q.counter - 1 }))))
-  .then(() => store.setter(KEY.increment, (_, a) => ({ counter: a.increment.counter - 1 })))
-  .then(() => store.setter(KEY.increment, (_, a) => (_, b) => ({ counter: b.increment.counter - 1 })))
-  .then(() => store.setter(KEY.timestamp, new Date().getTime()))
-
-
-setImmediate(() => {
-  assert(value === 0)
-  assert(typeof timestamp === 'number' && timestamp > 0)
-  console.log('[passed all the tests.]')
 })
